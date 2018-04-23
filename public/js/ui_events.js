d4e3471c9f36ede58e9fe2a7e73630a8d059e9c7
Kusuma Ramaiah <kusumaramaiah@in.ibm.com>
2018-04-23 12:41:54 +0530

    ID value added to span tag

--- public/js/ui_events.js
+++ public/js/ui_events.js
@@ -74,9 +74,7 @@ $(document).on('ready', function () {
 	$(document).on('click', '.colorOption', function () {
 		var color = $(this).attr('color');
 		var model = $(this).attr('id');
-		alert('I have got the ID value' + model);
-		var html = '<span class="fa fa-circle colorSelected ' + color + '" color="' + color + '"></span>';
-
+		var html = '<span class="fa fa-circle colorSelected ' + color + '" color="' + color + '" id="' + model + '"></span>';
 		$(this).parent().parent().find('.colorValue').html(html);
 		$(this).parent().hide();
 
